76f0ed135d65b12c7cd2bd1109b64bc9f1ad8688
Axel Nana <axel.nana@aliens-group.com>
2025-08-07 14:40:24 +0100

    feat: Add packages for codec plugins.

--- packages/a/amplitude-plugin-vorbis/xmake.lua
+++ packages/a/amplitude-plugin-vorbis/xmake.lua
@@ -12,33 +12,18 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-package("amplitudeaudiosdk")
+package("amplitude-plugin-vorbis")
   set_homepage("https://amplitudeaudiosdk.com/")
-  set_description("A cross-platform audio engine designed with the needs of games in mind.")
+  set_description("The official Amplitude plugin to encode and decode OGG/Vorbis audio files.")
   set_license("Apache-2.0")
 
-  set_urls("https://github.com/AmplitudeAudio/sdk.git")
+  set_urls("https://github.com/AmplitudeAudio/plugin-vorbis.git")
 
   add_configs("shared", { description = "Build shared library.", default = true, type = "boolean" })
 
-  add_versions("1.0.0", "feat/xmake")
+  add_versions("0.5.0", "main")
 
   on_install(function(package)
-    import("xmake.cpu", { rootdir = path.join(package:cachedir(), "source", "amplitudeaudiosdk") })
-    import("xmake.platform", { rootdir = path.join(package:cachedir(), "source", "amplitudeaudiosdk") })
-
-    local archs = cpu.am_get_supported_archs()
-
-    for _, arch in ipairs(archs) do
-      local _, defines, _ = cpu.am_get_arch_info(arch)
-
-      for _, define in ipairs(defines) do
-        package:add("defines", define)
-      end
-    end
-
-    platform.am_apply_detected_platform_defines(package)
-
     import("package.tools.xmake").install(package,
       { kind = package:config("shared") and "shared" or "static", as_package = true })
   end)
