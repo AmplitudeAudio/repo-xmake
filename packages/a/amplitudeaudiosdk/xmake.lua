a3212e2321a171313716c97e32b26d4ed122ad44
Axel Nana <axel.nana@aliens-group.com>
2025-08-07 01:08:28 +0100

    fix: Improved SDK package definition.

--- packages/a/amplitudeaudiosdk/xmake.lua
+++ packages/a/amplitudeaudiosdk/xmake.lua
@@ -13,17 +13,32 @@
 -- limitations under the License.
 
 package("amplitudeaudiosdk")
-  set_kind("library")
   set_homepage("https://amplitudeaudiosdk.com/")
   set_description("A cross-platform audio engine designed with the needs of games in mind.")
 
-  set_urls("https://github.com/AmplitudeAudio/sdk.git", { excludes = { "*/samples/*", "*/test/*" } })
+  set_urls("https://github.com/AmplitudeAudio/sdk.git")
 
   add_configs("shared", { description = "Build shared library.", default = true, type = "boolean" })
 
   add_versions("1.0.0", "feat/xmake")
 
   on_install(function(package)
-    import("package.tools.xmake").install(package, { kind = package:config("shared") and "shared" or "static" })
+    import("xmake.cpu", { rootdir = path.join(package:cachedir(), "source", "amplitudeaudiosdk") })
+    import("xmake.platform", { rootdir = path.join(package:cachedir(), "source", "amplitudeaudiosdk") })
+
+    local archs = cpu.am_get_supported_archs()
+
+    for _, arch in ipairs(archs) do
+      local _, defines, _ = cpu.am_get_arch_info(arch)
+
+      for _, define in ipairs(defines) do
+        package:add("defines", define)
+      end
+    end
+
+    platform.am_apply_detected_platform_defines(package)
+
+    import("package.tools.xmake").install(package,
+      { kind = package:config("shared") and "shared" or "static", as_package = true })
   end)
 package_end()
